d9334faa16d31635e11edc5e6d0d8fc7baf74bf7
Omer Sofer <omer.sofer@intel.com>
2017-06-12 23:40:29 +0300

    unit tests for "admin_panel" and "response" apps were added

--- admin_panel/tests.py
+++ admin_panel/tests.py
@@ -1,6 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from response.models import Mode
 
-# Create your tests here.
+
+class AdminPanelTestCase(TestCase):
+    def setUp(self):
+        Mode.objects.create()
+        # log in as an admin (for is_authenticated() check)
+        self.client = Client()
+        self.my_admin = User(username='admin', is_staff=True)
+        self.my_admin.set_password('adminadmin')  # can't set above because of hashing
+        self.my_admin.save()  # save to temporary test db
+        response = self.client.get('/admin/', follow=True)
+        loginresponse = self.client.login(username='admin', password='adminadmin')
+        self.assertTrue(loginresponse)  # check that logged in
+
+    def testChaosModeChanges(self):
+        # Test changing chaos_mode to "failure" (admin_panel.views.failure)
+        response = self.client.get('/admin/failure/')
+        self.assertEqual(Mode.objects.get(id=1).chaos_mode, 2)
+        # Test changing chaos_mode to "degraded" (admin_panel.views.degraded)
+        response = self.client.get('/admin/degraded/')
+        self.assertEqual(Mode.objects.get(id=1).chaos_mode, 1)
+        # Test changing chaos_mode to "normal" (admin_panel.views.normal)
+        response = self.client.get('/admin/normal/')
+        self.assertEqual(Mode.objects.get(id=1).chaos_mode, 0)
+
+
+
+
--- response/tests.py
+++ response/tests.py
@@ -1,6 +1,62 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from response.models import Mode
 
-# Create your tests here.
+
+def change_chaos_mode(new_mode):
+    mode = Mode.objects.get(id=1)
+    mode.chaos_mode = new_mode
+    mode.save()
+
+
+class ResponseTestCase(TestCase):
+    def setUp(self):
+        Mode.objects.create()
+
+    def testNormalModeResponses(self):
+        # set chaos_mode=normal
+        change_chaos_mode(0)
+        http200Count = 0
+        for a in range(0, 100):
+            response = self.client.get('/response/')
+            if response.status_code == 200:
+                http200Count += 1
+        # Check that all responses are 200 http responses
+        self.assertTrue(http200Count == 100)
+
+    def testDegradedModeResponses(self):
+        # set chaos_mode=degraded
+        change_chaos_mode(1)
+        http200Count = 0
+        http401Count = 0
+        http500Count = 0
+        for a in range(0, 100):
+            response = self.client.get('/response/')
+            if response.status_code == 200:
+                http200Count += 1
+            if response.status_code == 401:
+                http401Count += 1
+            if response.status_code == 500:
+                http500Count += 1
+        # Check number of each http response
+        self.assertTrue(http200Count > 40 and http200Count < 60) # should be 50, check that in a reasonable range
+        self.assertTrue(http401Count > 15 and http401Count < 35) # should be 25, check that in a reasonable range
+        self.assertTrue(http500Count > 15 and http500Count < 35) # should be 25, check that in a reasonable range
+
+    def testFailureModeResponses(self):
+        # set chaos_mode=failure
+        change_chaos_mode(2)
+        http200Count = 0
+        http500Count = 0
+        for a in range(0, 100):
+            response = self.client.get('/response/')
+            if response.status_code == 200:
+                http200Count += 1
+            if response.status_code == 500:
+                http500Count += 1
+        # Check number of each http response
+        self.assertTrue(http200Count < 15) #should be 5, check that in a reasonable range
+        self.assertTrue(http500Count > 85) #should be 95, check that in a reasonable range
