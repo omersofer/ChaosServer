395c68344afb42b32b38f10f6e7eee94c49c20d8
Omer Sofer <omer.sofer@intel.com>
2017-06-13 10:21:59 +0300

    response unit test modified to be more generic

--- response/tests.py
+++ response/tests.py
@@ -1,62 +1,78 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.test import TestCase
-from django.test import Client
-from django.contrib.auth.models import User
 from response.models import Mode
 
+# Global variables
+normal200ReqPercent = 100
+normal401ReqPercent = 0
+normal500ReqPercent = 0
 
+degraded200ReqPercent = 50
+degraded401ReqPercent = 25
+degraded500ReqPercent = 25
+
+failure200ReqPercent = 5
+failure401ReqPercent = 0
+failure500ReqPercent = 95
+
+oneSideDelta = 10
+
+# Help functions
 def change_chaos_mode(new_mode):
     mode = Mode.objects.get(id=1)
     mode.chaos_mode = new_mode
     mode.save()
 
+def probeResponseEndpoint(self, chaos_mode):
+    http200Count = 0
+    http401Count = 0
+    http500Count = 0
+    for a in range(0, 100):
+        response = self.client.get('/response/')
+        if response.status_code == 200:
+            http200Count += 1
+        if response.status_code == 401:
+            http401Count += 1
+        if response.status_code == 500:
+            http500Count += 1
+    # Set required percentages according to chaos_mode
+    mode200ReqPercent = normal200ReqPercent
+    mode401ReqPercent = normal401ReqPercent
+    mode500ReqPercent = normal500ReqPercent
+    if (chaos_mode == 1):  # degraded case
+        mode200ReqPercent = degraded200ReqPercent
+        mode401ReqPercent = degraded401ReqPercent
+        mode500ReqPercent = degraded500ReqPercent
+    elif (chaos_mode == 2):  # failure casse
+        mode200ReqPercent = failure200ReqPercent
+        mode401ReqPercent = failure401ReqPercent
+        mode500ReqPercent = failure500ReqPercent
+    # Check percent of each http response is in a reasonable delta from required percentages
+    self.assertTrue((http200Count > (mode200ReqPercent-oneSideDelta)) and (http200Count < (mode200ReqPercent+oneSideDelta)))
+    self.assertTrue((http401Count > (mode401ReqPercent-oneSideDelta)) and (http401Count < (mode401ReqPercent+oneSideDelta)))
+    self.assertTrue((http500Count > (mode500ReqPercent-oneSideDelta)) and (http500Count < (mode500ReqPercent+oneSideDelta)))
 
+
+# Test class
 class ResponseTestCase(TestCase):
     def setUp(self):
         Mode.objects.create()
 
     def testNormalModeResponses(self):
-        # set chaos_mode=normal
+        # Set chaos_mode=normal
         change_chaos_mode(0)
-        http200Count = 0
-        for a in range(0, 100):
-            response = self.client.get('/response/')
-            if response.status_code == 200:
-                http200Count += 1
-        # Check that all responses are 200 http responses
-        self.assertTrue(http200Count == 100)
+        # check that get the required responses percentages
+        probeResponseEndpoint(self, 0)
 
     def testDegradedModeResponses(self):
         # set chaos_mode=degraded
         change_chaos_mode(1)
-        http200Count = 0
-        http401Count = 0
-        http500Count = 0
-        for a in range(0, 100):
-            response = self.client.get('/response/')
-            if response.status_code == 200:
-                http200Count += 1
-            if response.status_code == 401:
-                http401Count += 1
-            if response.status_code == 500:
-                http500Count += 1
-        # Check number of each http response
-        self.assertTrue(http200Count > 40 and http200Count < 60) # should be 50, check that in a reasonable range
-        self.assertTrue(http401Count > 15 and http401Count < 35) # should be 25, check that in a reasonable range
-        self.assertTrue(http500Count > 15 and http500Count < 35) # should be 25, check that in a reasonable range
+        # check that get the required responses percentages
+        probeResponseEndpoint(self, 1)
 
     def testFailureModeResponses(self):
         # set chaos_mode=failure
         change_chaos_mode(2)
-        http200Count = 0
-        http500Count = 0
-        for a in range(0, 100):
-            response = self.client.get('/response/')
-            if response.status_code == 200:
-                http200Count += 1
-            if response.status_code == 500:
-                http500Count += 1
-        # Check number of each http response
-        self.assertTrue(http200Count < 15) #should be 5, check that in a reasonable range
-        self.assertTrue(http500Count > 85) #should be 95, check that in a reasonable range
+        # check that get the required responses percentages
+        probeResponseEndpoint(self, 2)
--- response/views.py
+++ response/views.py
@@ -4,30 +4,43 @@ from django.http import HttpResponse
 from response.models import Mode
 from random import randint
 
+# Global variables
+normal200ReqPercent = 100
+normal401ReqPercent = 0
+normal500ReqPercent = 0
+
+degraded200ReqPercent = 50
+degraded401ReqPercent = 25
+degraded500ReqPercent = 25
+
+failure200ReqPercent = 5
+failure401ReqPercent = 0
+failure500ReqPercent = 95
+
 
 def index(request):
     mode = Mode.objects.get(id=1)
     rand = (randint(1,100))
 
-    #"normal" mode: 100% of requests will result in 200 http responses
+    # "normal" mode: 100% of requests will result in 200 http responses
     if mode.chaos_mode == 0:
         return HttpResponse(status=200)
 
-    #"degraded" mode:   50% of requests will result in 200 http responses
-    #                   25% of requests will result in 401 http responses
-    #                   25% of requests will result in 500 http responses
+    # "degraded" mode:   50% of requests will result in 200 http responses
+    #                    25% of requests will result in 401 http responses
+    #                    25% of requests will result in 500 http responses
     elif mode.chaos_mode == 1:
-        if rand <= 50:
+        if rand <= degraded200ReqPercent:
             return HttpResponse(status=200)
-        elif rand <= 75:
+        elif rand <= (degraded200ReqPercent+degraded401ReqPercent):
             return HttpResponse(status=401)
         else:
             return HttpResponse(status=500)
 
-    #"failure" mode:    5%  of requests will result in 200 http responses
+    # "failure" mode:    5%  of requests will result in 200 http responses
     #                   95% of requests will result in 500 http responses
     else:
-        if rand <= 5:
+        if rand <= failure200ReqPercent:
             return HttpResponse(status=200)
         else:
             return HttpResponse(status=500)
